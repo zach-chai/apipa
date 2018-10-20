c378a8828377d2e717f8a9f4ea3ab8895ae0b48c
Zachary Chai <zachary.chai@outlook.com>
2018-10-20 11:49:51 -0400

    add message routes

--- app.rb
+++ app.rb
@@ -5,3 +5,15 @@ set :bind, '0.0.0.0'
 get '/' do
   'Hello world!'
 end
+
+get '/messages' do
+  # TODO implement
+end
+
+get '/messages/:id' do
+  # TODO implement
+end
+
+post '/messages' do
+  # TODO implement
+end
